d763c9045121ccac709177d76e0b1f391348f709
Sven Boemer <sbomer@gmail.com>
2017-08-11 16:39:35 -0700

    fix path to pipeline script

--- buildpipeline/pipelinejobs.groovy
+++ buildpipeline/pipelinejobs.groovy
@@ -1,6 +1,6 @@
 import org.dotnet.ci.pipelines.Pipeline
 
-def testPipeline = Pipeline.createPipeline(this, 'test-pipeline.groovy')
+def testPipeline = Pipeline.createPipeline(this, 'buildpipeline/test-pipeline.groovy')
 testPipeline.triggerPipelineManually()
 testPipeline.triggerPipelineOnPush()
 testPipeline.triggerPipelineOnEveryGithubPR('test pipeline status', ".*test\\W+my\\W+job.*")
