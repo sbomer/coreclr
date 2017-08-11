3045f8aff821dcbeae8d375ced6e4b05d52166d8
Sven Boemer <sbomer@gmail.com>
2017-08-11 14:06:01 -0700

    use github-specific trigger with trigger phrase

--- buildpipeline/pipelinejobs.groovy
+++ buildpipeline/pipelinejobs.groovy
@@ -3,4 +3,4 @@ import org.dotnet.ci.pipelines.Pipeline
 def testPipeline = Pipeline.createPipeline(this, 'test-pipeline.groovy')
 testPipeline.triggerPipelineManually()
 testPipeline.triggerPipelineOnPush()
-testPipeline.triggerPipelineOnEveryPR('test pipeline status', ".*test\\W+my\\W+job.*")
+testPipeline.triggerPipelineOnEveryGithubPR('test pipeline status', ".*test\\W+my\\W+job.*")
