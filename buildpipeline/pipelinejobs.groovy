2c4c453fa66a0fa44284cb6d3e94276521d13b36
Sven Boemer <sbomer@gmail.com>
2017-08-14 14:14:24 -0700

    fix trigger

--- buildpipeline/pipelinejobs.groovy
+++ buildpipeline/pipelinejobs.groovy
@@ -6,4 +6,4 @@ testPipeline.triggerPipelineOnPush()
 testPipeline.triggerPipelineOnEveryGithubPR('test pipeline status', ".*test\\W+my\\W+job.*")
 
 def testPipeline2 = Pipeline.createPipeline(this, 'buildpipeline/test-pipeline2.groovy')
-testPipeline.triggerPipelineManually()
+testPipeline2.triggerPipelineManually()
