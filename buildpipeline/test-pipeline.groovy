2ebff728c0a1b0e5ca9fb06ff84bc489f5acbbba
Sven Boemer <sbomer@gmail.com>
2017-08-15 09:38:44 -0700

    add checkout step

--- buildpipeline/test-pipeline.groovy
+++ buildpipeline/test-pipeline.groovy
@@ -2,9 +2,10 @@
 
 simpleNode('Windows_NT', 'latest') {
     stage('checkout sources') {
+        checkout scm
     }
     stage('init test dependencies') {
-        bat '.\\init-tools.cmd'
+        bat 'init-tools.cmd'
         bat 'python tests\\scripts\\build_jitutils.py'
     }
     stage('obtain artifacts') {
