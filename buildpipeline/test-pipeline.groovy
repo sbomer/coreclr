ec6543f7cd00c5b75a8a28cfded67369234ab36f
Sven Boemer <sbomer@gmail.com>
2017-08-15 09:37:48 -0700

    remove steps

--- buildpipeline/test-pipeline.groovy
+++ buildpipeline/test-pipeline.groovy
@@ -4,12 +4,8 @@ simpleNode('Windows_NT', 'latest') {
     stage('checkout sources') {
     }
     stage('init test dependencies') {
-        step('init-tools') {
-            bat '.\\init-tools.cmd'
-        }
-        step('build jitutils') {
-            bat 'python tests\\scripts\\build_jitutils.py'
-        }
+        bat '.\\init-tools.cmd'
+        bat 'python tests\\scripts\\build_jitutils.py'
     }
     stage('obtain artifacts') {
         bat 'Tools\\jitutils\\bin\\cijobs --help'
