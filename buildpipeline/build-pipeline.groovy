702f3454dd6351cd20d4ac6e4d52515817e9b845
Sven Boemer <sbomer@gmail.com>
2017-08-17 09:56:51 -0700

    test parameterized build job

--- buildpipeline/build-pipeline.groovy
+++ buildpipeline/build-pipeline.groovy
@@ -14,33 +14,40 @@
 // os, arch, config, commit, coreclr, owner, branch
 
 
-simpleNode('Windows_NT', 'latest') {
+simpleNode(params.os, 'latest') {
     stage('checkout sources') {
         checkout scm
     }
     stage('build') {
         // bat '.\\build.cmd'
-        bat 'print building! > build.txt'
-        bat 'mkdir bin'
-        bat 'print build file > bin/build.txt'
+        if (params.os == "Windows_NT") {
+            bat 'print building on windows! > build.txt'
+            bat 'mkdir bin'
+            bat 'print build file > bin/build.txt'
+        } else if (params.os == "Ubuntu") {
+            sh 'echo building on ubuntu! > build.txt'
+            sh 'mkdir bin'
+            sh 'echo build file > bin/build.txt'
+        }
+        
     }
     stage('archive artifacts') {
         archiveArtifacts artifacts: 'build.txt'
         archiveArtifacts artifacts: 'bin/*'
     }
 }
-stage('parallel test') {
-    parallel (
-        "windows test" : {
-            simpleNode('Windows_NT', 'latest') {
-                bat "print from windows"
-            }
-        },
-        "linux test" : {
-            simpleNode('Ubuntu14.04', 'latest') {
-                sh "echo from linux"
-            }
-        }
-    )
-}
-
+// stage('parallel test') {
+//     parallel (
+//         "windows test" : {
+//             simpleNode('Windows_NT', 'latest') {
+//                 bat "print from windows"
+//             }
+//         },
+//         "linux test" : {
+//             simpleNode('Ubuntu14.04', 'latest') {
+//                 sh "echo from linux"
+//             }
+//         }
+//     )
+// }
+// 
